441662afcbc69585b39abf44bb6eb468556a79f6
Alexander Tsoy <cori7gorilolik@gmail.com>
2025-10-20 20:47:06 +0500

    Add Render domain to ALLOWED_HOSTS

--- Anime_front/settings.py
+++ Anime_front/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-is8tht!afy&!7)*l8%27-b1n@qwx2ag!eby4!&@b@5w=977vx@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
 
 
 # Application definition
